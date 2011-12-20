fe19d41fc1074b0a1c8ac2368828acf3a6823549
Matt Brand <agmattbrand@yahoo.com>
2011-12-20 11:27:50 -0500

    figuring out the configuration and helpers

--- lib/wizard_of_awes.rb
+++ lib/wizard_of_awes.rb
@@ -3,5 +3,52 @@ require 'wizard_of_awes/configuration'
 require 'wizard_of_awes/helpers'
 
 module WizardOfAwes
-  # Your code goes here...
+  def self.frameworks
+    frameworks = ['rails']
+    frameworks
+  end
+
+  def self.load_framework!
+    show_warning if frameworks.empty?
+    frameworks.each do |framework|
+      begin
+        require framework
+      rescue NameError => e
+        raise "Failed to load framework #{framework.inspect}. Have you added it to Gemfile?"
+      end
+    end
+  end
+  
+   def self.show_warning
+      $stderr.puts <<-EOC
+  warning: no framework detected.
+  would you check out if your Gemfile appropriately configured?
+  ---- e.g. ----
+  when Rails:
+  gem 'rails'
+  gem 'wizard_of_awes'
+
+  EOC
+    end
+      
+    def self.load_wizard_of_awes!
+      require 'wizard_of_awes/config'
+      require 'wizard_of_awes/helpers'
+    end    
+
+    def self.hook!
+      load_framework!
+      load_wizard_of_awes!
+    end
+
+    def self.load!
+      hook!
+    end
+
+    private
+    def self.rails?
+      defined?(::Rails)
+    end
 end
+
+WizardOfAwes.load!
