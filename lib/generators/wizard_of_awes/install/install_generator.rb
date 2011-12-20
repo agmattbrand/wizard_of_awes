c2d1a9fc9761a3bb0a6ecf5c807bdbee51b1416c
Matt Brand <agmattbrand@yahoo.com>
2011-12-20 10:52:36 -0500

    moving files around to get the build right

--- lib/generators/wizard_of_awes/install/install_generator.rb
+++ lib/generators/wizard_of_awes/install/install_generator.rb
@@ -1,25 +1,26 @@
+require 'rails/generators'
+require 'rails/generators/migration'
+require 'rails/generators/active_record/migration'
+
 module WizardOfAwes
-  module Generators
-    class InstallGenerator < Rails::Generators::Base
-      desc "Installs Wizard Of Awes and generates the migration"
+  class InstallGenerator < Rails::Generators::Base
+    desc "Installs Wizard Of Awes and generates the migration"
 
-      include Rails::Generators::Migration
+    include Rails::Generators::Migration
+    extend ActiveRecord::Generators::Migration
 
-      def self.next_migration_number(dirname)
-        Time.now.strftime("%Y%m%d%H%M%S")
-      end
+    source_root File.expand_path('../templates', __FILE__)
 
-      def copy_initializer
-        template 'wizard_of_awes.rb.erb', 'config/initializers/wizard_of_awes.rb'
-      end
+    def self.next_migration_number(dirname)
+      Time.now.strftime("%Y%m%d%H%M%S")
+    end
 
-      def create_migrations
-        Dir["#{self.class.source_root}/migrations/*.rb"].sort.each do |filepath|
-          name = File.basename(filepath)
-          migration_template "migrations/#{name}", "db/migrate/#{name.gsub(/^\d+_/,'')}"
-          sleep 1
-        end
-      end
+    def copy_initializer
+      template 'wizard_of_awes.rb.erb', 'config/initializers/wizard_of_awes.rb'
+    end
+
+    def create_migration_file
+      migration_template 'create_helper_snippets.rb', 'db/migrate/create_helper_snippets.rb'      
     end
   end
 end
