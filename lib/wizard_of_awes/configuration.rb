0d054065866aa663b05c59a3310f5f8d0778eaf3
Matt Brand <agmattbrand@yahoo.com>
2011-12-20 11:09:29 -0500

    this is a better way to setup the configuration

--- lib/wizard_of_awes/configuration.rb
+++ lib/wizard_of_awes/configuration.rb
@@ -1,25 +1,28 @@
-class WizardOfAwes
-  class Configuration
+require 'active_support/configurable'
 
-    attr_accessor :snippet_route_prefix, :current_user_method, :authentication_method
+module WizardOfAwes
+  def self.configure(&block)
+    yield @config ||= Kaminari::Configuration.new
+  end
+  
+  def self.config
+    @config
+  end
+  
+  class Configuration #:nodoc:
+    include ActiveSupport::Configurable
+    config_accessor :snippet_route_prefix
+    config_accessor :current_user_method
+    config_accessor :authentication_method
 
-    def initialize
-      @snippet_route_prefix = 'woa-snippets'
-      @current_user_method = :current_helper_user
-      @authentication_method = :authenticate_helper_user!
+    def param_name
+      config.param_name.respond_to?(:call) ? config.param_name.call : config.param_name
     end
+  end  
 
-  end
-
-  class << self
-    attr_accessor :configuration
-  end
-
-  def self.configuration
-    @configuration ||= Configuration.new
-  end
-
-  def self.configure
-    yield(configuration)
+  configure do |config|
+    config.snippet_route_prefix = 'woa-snippets'
+    config.current_user_method = :current_helper_user
+    config.authentication_method = :authenticate_helper_user!
   end
 end
